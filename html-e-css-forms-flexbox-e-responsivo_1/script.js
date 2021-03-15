845df2140e977670bf156e93e04dcdea1ae75fa5
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-15 17:51:55 -0300

    Atualiza exercicio

--- html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -8,6 +8,42 @@ function createStateOptions() {
   }
 }
 
+// function renderCurriculum(event) {
+//   event.preventDefault();
+//   const formElements = document.querySelectorAll('input');
+//   for (let index = 0; index < formElements.length; index += 1) {
+//     if (formElements[index].type === 'radio' && !formElements[index].checked) {
+//       continue;
+//     }
+//     const userInput = formElements[index].value;
+//     const dataUser = document.querySelector('.form-data');
+//     if (checkEmail() && checkData()) {
+//       const div = document.createElement('div');
+//       div.className = 'div-curriculum';
+//       div.innerHTML = userInput;
+//       dataUser.appendChild(div);
+//     }
+//   }
+// }
+
+// const clearButton = document.querySelector('.clear-button');
+// function clearFields() {
+//   const formElements = document.querySelectorAll('input');
+//   const textArea = document.querySelector('textarea')
+//   const div = document.querySelectorAll('.div-curriculum');
+//   for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+//     const userInput = formElements[index];
+//     userInput.value = '';
+//     textArea.value = '';
+//     div[index].innerText = '';
+//   }
+// }
+
+// const submitButton = document.querySelector('.submit-button');
+// submitButton.addEventListener('click', renderCurriculum);
+// clearButton.addEventListener('click', clearFields);
+
+
 // function validateData(data) {
 //   if (data.indexOf('/') === 2 || data.indexOf('/') === 5) {
 //     const day = data.substr(0, 2);
